8ce4570b768edae747031939ccddf58d06670581
Sawyer Andrews <hi@sawyer.systems>
2026-06-15 15:44:28 -0500

    i may be washed

--- extension/src/handlers/fishgoo.js
+++ extension/src/handlers/fishgoo.js
@@ -2,12 +2,27 @@ export function getOriginalFromFishgoo(url) {
   try {
     const parsedUrl = new URL(url);
 
-    const searchUrl = parsedUrl.searchParams.get("productLink");
+    // Try normal search params first
+    let searchUrl = parsedUrl.searchParams.get("productLink");
     if (searchUrl) {
       return { searchUrl };
+    }
+
+    // Try hash search params if fishgoo uses hash routing
+    if (parsedUrl.hash) {
+      const hashQueryIndex = parsedUrl.hash.indexOf("?");
+      if (hashQueryIndex !== -1) {
+        const hashQueryString = parsedUrl.hash.substring(hashQueryIndex + 1);
+        const hashSearchParams = new URLSearchParams(hashQueryString);
+        searchUrl = hashSearchParams.get("productLink");
+        if (searchUrl) {
+          return { searchUrl };
+        }
+      }
     }
   } catch (error) {
     return null;
   }
   return null;
 }
+
